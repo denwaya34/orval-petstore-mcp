c8d29699452fe2e09a2a284bbce3c0803c244cb6
denwaya34 <denwaya34@gmail.com>
2025-05-05 00:29:45 +0900

    init

---
+++ orval.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'orval';
+
+export default defineConfig({
+    petstore: {
+        input: {
+            target: './petstore.yaml',
+        },
+        output: {
+            mode: 'single',
+            client: 'mcp',
+            baseUrl: 'https://petstore3.swagger.io/api/v3',
+            target: 'src/handlers.ts',
+            schemas: 'src/http-schemas',
+        },
+    },
+});
