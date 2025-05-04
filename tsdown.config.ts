6ee74e79eb8f4f35648c26574ffe879301004908
denwaya34 <denwaya34@gmail.com>
2025-05-05 00:41:50 +0900

    init

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsdown'
 
 export default defineConfig({
-    entry: ['./src/index.ts'],
+    entry: ['./src/server.ts'],
     target: 'node20.18',
     clean: true,
     dts: true,
